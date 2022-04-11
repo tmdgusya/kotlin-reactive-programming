4f3d2bac14f37544c448848fa022b2b154cdfb1e
tmdgusya <tmdgusya@naver.com>
2022-04-11 22:40:21 +0900

    기존의 반복자(iterator) 를 이용한 패턴

--- src/main/kotlin/chapter01/Example01.kt
+++ src/main/kotlin/chapter01/Example01.kt
@@ -7,6 +7,10 @@ fun main() {
     var list = listOf("One", 2, "Three", "Four", 4.5, "Five")
 
     var iterator = list.iterator()
+
+    /**
+     * Iterator 를 도는 동안 Thread 는 Blocking 된다.
+     */
     while (iterator.hasNext()) {
         println(iterator.next())
     }
